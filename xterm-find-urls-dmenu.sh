8f2a0ceef441ce5bb732ee861ba62e6301e49f6f
Anton Kindestam <antonki@kth.se>
2018-12-11 16:22:29 +0100

    xterm-find-urls-dmenu.sh: Add comment documenting my own stupidity
    Totally overengineered... I guess it's useful if I ever change the URL
    regex.
    
    A better solution (read: even more overengineered) could be to
    urlencode the URL as it comes out of dmenu.

--- xterm-find-urls-dmenu.sh
+++ xterm-find-urls-dmenu.sh
@@ -20,6 +20,8 @@ grep --text -Poi "$URL_REGEX" "$TEMP_PRINT" | uniq | tail -n "$MAXURL" > "$TEMP_
 URL="$(tac "$TEMP_URLS" | $DMENU -l "$(wc -l < "$TEMP_URLS")")"
 [ $? -eq 0 ] || exit
 
+# With the current regex there's no way we'll actually have any single
+# quotes in $URL, so this is actually totally overkill.
 escape()
 {
     echo "$1" | sed s/\'/\'\\\\\'\'/g
